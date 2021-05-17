4e950f54ac51d3471877cbfbd45eb725e30a73c8
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-17 14:36:01 +0400

    Create(1. Object with getter and setter): Created file and resolved task

--- src/1.objWithGetSet.js
+++ src/1.objWithGetSet.js
@@ -2,9 +2,10 @@ const obj = {
   _name: [], // ['name', length][]
   set name(name) {
     const names = name.split(', ')
-    names.forEach(element => {
+
+    names.forEach((element) => {
       this._name.push([element, element.length])
-    });
+    })
   },
   get name() {
     return this._name
@@ -12,5 +13,5 @@ const obj = {
 }
 
 console.log(obj.name)
-obj.name = 'Vrezh, Artavazd, Inga';
+obj.name = 'Vrezh, Artavazd, Inga'
 console.log(obj.name) // [['Vrezh', 7], ['Artavazd', 10]]

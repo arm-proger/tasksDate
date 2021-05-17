a5fea8c8529fbb15f2128450c15f8ca06421b263
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-17 21:09:32 +0400

    Create(5. Get week name): Created file and resolved task

---
+++ src/5.getWeekName.js
@@ -0,0 +1,21 @@
+const weekNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday", 
+  "Friday",
+  "Saturday"
+]
+
+
+const getWeekName = (date) => {
+  const thisDate = new Date(date.trim()),
+        getThisWeek = thisDate.getDay()
+
+  return weekNames[getThisWeek]
+}
+
+console.log(getWeekName("12/07/2016")) // Wednesday
+console.log(getWeekName("09/04/2016")) // Sunday
+console.log(getWeekName("12/08/2011")) // Thursday

47b323f48fc865133fae594691d51150d5829f48
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-17 22:27:17 +0400

    Create(3. Get months): Created file and resolved task

--- src/3.getMonths.js
+++ src/3.getMonths.js
@@ -22,7 +22,7 @@ function monthsInterval(startMonth, endMonth) {
   const minMonth = minDate.getMonth()
 
   // Get distance of months
-  const distanceMonth = Math.floor((maxDate - minDate) / 2628e6)
+  const distanceMonth = Math.round((maxDate - minDate) / 2628e6)
   const months = []
 
   for (let i = 0; i <= distanceMonth; i++) {
@@ -35,10 +35,14 @@ function monthsInterval(startMonth, endMonth) {
   return months
 }
 
-let january = new Date(2017, 0, 1)
-let march = new Date(2017, 2, 1)
-console.log(monthsInterval(january, march))
+// let january = new Date(2017, 0, 1)
+// let march = new Date(2017, 2, 1)
+// console.log(monthsInterval(january, march))
 
-// let january2017 = new Date(2017, 0, 1);
-// let january2018 = new Date(2018, 0, 1);
+let january2017 = new Date(2017, 11, 1)
+let january2018 = new Date(2018, 0, 1)
+console.log(monthsInterval(january2017, january2018))
+
+// let january2017 = new Date(2017, 0, 1)
+// let january2018 = new Date(2018, 0, 1)
 // console.log(monthsInterval(january2017, january2018))
